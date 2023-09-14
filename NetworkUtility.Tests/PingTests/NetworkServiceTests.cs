c5c26bb80d90bfb67afbb8f228d417c768717a52
OLSRenanSouza <renantorres@ols-br.com>
2023-09-14 09:44:15 -0300

    first commit

---
+++ NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
@@ -0,0 +1,95 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using FluentAssertions.Extensions;
+using NetworkUtility.DNS;
+using NetworkUtility.Ping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NetworkUtility.Tests.PingTests
+{
+    public class NetworkServiceTests
+    {
+        private readonly NetworkService _networkService;
+        private readonly IDNS _dNS;
+        public NetworkServiceTests()
+        {
+            _dNS = A.Fake<IDNS>();
+            _networkService = new NetworkService(_dNS);
+        }
+        [Fact]
+        public void NetworkService_SendPing_ReturnString()
+        {
+            A.CallTo(() => _dNS.SendDNS()).Returns(false);
+            //Act
+            var result = _networkService.SendPing();
+
+            //Assert
+            result.Should().NotBeNullOrWhiteSpace();
+            result.Should().Be("Ping");
+            result.Should().Contain("Pin", Exactly.Once());
+        }
+
+        [Fact]
+        public void NetworkService_LastPingDate_ReturnString()
+        {
+            //Act
+            var result = _networkService.LastPingDate();
+
+            //Assert
+            result.Should().BeAfter(1.January(2010));
+            result.Should().BeBefore(1.January(2030));
+        }
+
+        [Fact]
+        public void NetworkService_GetPingOptions_ReturnObject()
+        {
+            //Act
+            var result = _networkService.GetPingOptions();
+            var expected = new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1,
+            };
+
+            //Assert
+            result.Should().BeOfType<PingOptions>();
+            result.Should().BeEquivalentTo(expected);
+            result.Ttl.Should().Be(1);
+        }
+
+        [Fact]
+        public void NetworkService_GetPingOptionsList_ReturnObjectList()
+        {
+            //Act
+            var result = _networkService.GetPingOptionsList();
+            var expected = new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1,
+            };
+
+            //Assert
+            //result.Should().BeOfType<PingOptions>();
+            result.Should().ContainEquivalentOf(expected);
+            result.Should().Contain(x => x.DontFragment == true);
+        }
+
+        [Theory]
+        [InlineData(1, 1, 2)]
+        [InlineData(2, 2, 4)]
+        public void NetworkService_PingTimeout_ReturnInt(int a, int b, int expected)
+        {
+            int result = _networkService.PingTimeout(a, b);
+
+            result.Should().Be(expected);
+        }
+
+    }
+
+}
---
+++ NetworkUtility/Ping/NetworkService.cs
@@ -0,0 +1,68 @@
+﻿using NetworkUtility.DNS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkUtility.Ping
+{
+    public class NetworkService
+    {
+        readonly IDNS _dnsService;
+        public NetworkService(IDNS dNS) 
+        {
+            _dnsService = dNS;
+        }
+        public string SendPing()
+        {
+            var dnsSuccess = _dnsService.SendDNS();
+            if (dnsSuccess)
+            {
+                return "Ping";
+            }
+            return "Ping failed";
+        }
+
+        public int PingTimeout(int a, int b) 
+        {
+            return a + b;
+        }
+
+        public PingOptions GetPingOptions() 
+        { 
+            return new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1,
+            };
+        }
+        public DateTime LastPingDate() 
+        { 
+            return DateTime.Now;
+        }
+        public IEnumerable<PingOptions> GetPingOptionsList() 
+        { 
+            IEnumerable<PingOptions> pingOptions = new List<PingOptions>()
+            {
+                new PingOptions()
+                {
+                    DontFragment = true,
+                    Ttl = 1,
+                },    
+                new PingOptions()
+                {
+                    DontFragment = true,
+                    Ttl = 1,
+                },     
+                new PingOptions()
+                {
+                    DontFragment = true,
+                    Ttl = 1,
+                },
+            };
+            return pingOptions;
+        }
+    }
+}
